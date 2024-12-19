d1a8c6698313c61235ae609013c1a63a606b0b64
Surya Prakash Yadav <suryaprakash8707yadav@gmail.com>
2024-12-19 15:43:27 +0530

    Add files via upload

---
+++ Codechef/Fitness.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+   int t;
+   scanf("%d",&t);
+   while(t--){
+       int x;
+       scanf("%d",&x);
+    
+           int z = 2*x;
+           printf("%d\n" , 5*z);
+       
+   }
+}
