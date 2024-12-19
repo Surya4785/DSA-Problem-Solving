d79712ad43f1571b74cd1e254124064dce103a0e
Surya Prakash Yadav <suryaprakash8707yadav@gmail.com>
2024-12-19 22:44:55 +0530

    Update RunningComparison.c

--- Codechef/RunningComparison.c
+++ Codechef/RunningComparison.c
@@ -1,9 +1,24 @@
-#include <stdio.h>
+/*RUNNING COMPARISON
+Alice and Bob recently got into running, and decided to compare how much they ran on different days. They each ran for N days
+On the ith day, Alice ran Ai meters and Bob ran Bi meters.
 
+On each day,
+    
+- Alice is unhappy if Bob ran strictly more than twice her distance, and happy otherwise.
+- Similarly, Bob is unhappy if Alice ran strictly more than twice his distance, and happy otherwise.
+
+For example, on a given day
+
+- If Alice ran 200 meters and Bob ran 500, Alice would be unhappy but Bob would be happy.
+- If Alice ran 500 meters and Bob ran 240, Alice would be happy and Bob would be unhappy.
+- If Alice ran 300 meters and Bob ran 500, Both ALice and Bob would be happy.
+
+    On how many of the N days were both Alice and Bob happy??  */
+
+#include <stdio.h>
 int main() {
     int t;
     scanf("%d", &t);
-
     while (t--) {
         int n;
         scanf("%d", &n);
@@ -24,7 +39,5 @@ int main() {
         }
         printf("%d\n",hD);
     }
-    
-
     return 0;
 }
