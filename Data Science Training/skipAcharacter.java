ce19eb789b392ad7e52d7e90a8752df87e6f4cc0
Surya Prakash Yadav <suryaprakash8707yadav@gmail.com>
2025-01-30 06:05:04 +0530

    To skip a character

--- Data Science Training/skipAcharacter.java
+++ Data Science Training/skipAcharacter.java
@@ -1,7 +1,6 @@
-//package String;
 import java.util.Arrays;
 import java.util.Scanner;
-public class skipAcharater {
+public class skipAcharacter {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str = sc.next();
