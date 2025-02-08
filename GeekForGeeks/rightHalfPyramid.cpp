8411f426962a60c86789fd9fbafc936759fb9e61
Surya Prakash Yadav <suryaprakash8707yadav@gmail.com>
2025-02-08 13:07:31 +0530

    Right Half Pyramid

---
+++ GeekForGeeks/rightHalfPyramid.cpp
@@ -0,0 +1,17 @@
+// To Print Right Half Pyramid Pattern using for loop
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int rows = 5;
+
+	for (int i = 1; i <= rows; i++) {
+		for (int j = 1; j <= i; j++) {
+		
+			cout << "* ";
+		}
+		cout << "\n";
+	}
+	return 0;
+}
