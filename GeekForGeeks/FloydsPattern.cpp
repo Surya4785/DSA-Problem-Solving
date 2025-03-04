bcac835ee76249da6e1e706317b0aaeb0fd6cd12
Surya Prakash Yadav <suryaprakash8707yadav@gmail.com>
2025-03-04 23:33:27 +0530

    Floyd's Pattern of Triangles

--- GeekForGeeks/FloydsPattern.cpp
+++ GeekForGeeks/FloydsPattern.cpp
@@ -4,20 +4,19 @@ using namespace std;
 
 void print_patt(int row)
 {
-	int count = 1; 
-	for (int i = 1; i <= row; i++) 
-	{
-		for (int j = 1; j <= i; j++) 
-		{
-			cout << count << " ";
-			count += 1;
-		}
-		cout << "\n";
-	}
-}
-int main()
+  int count = 1; 
+  for (int i = 1; i <= row; i++)  {
+	for (int j = 1; j <= i; j++) 
+	  {
+	    cout << count << " ";
+	    count += 1;
+	  }
+	    cout << "\n";
+	  }
+ }
+  int main()
 {
-	int row = 5;
-	print_patt(row);
-	return 0;
+   int row = 5;
+   print_patt(row);
+   return 0;
 }
