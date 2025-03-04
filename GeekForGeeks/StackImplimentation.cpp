41c24e57a245a9adac8818fe6de5b166eb56e608
Surya Prakash Yadav <suryaprakash8707yadav@gmail.com>
2025-03-04 23:48:20 +0530

    Stack Implimentation

--- GeekForGeeks/StackImplimentation.cpp
+++ GeekForGeeks/StackImplimentation.cpp
@@ -2,71 +2,61 @@
 using namespace std; 
 
 class twoStacks { 
-	int* arr; 
-	int size; 
-	int top1, top2; 
+int* arr; 
+int size; 
+int top1, top2; 
 
 public: 
-	twoStacks(int n) 
-	{ 
-		size = n; 
-		arr = new int[n]; 
-		top1 = n / 2 + 1; 
-		top2 = n / 2; 
-	} 
-	void push1(int x) 
-	{ 
-		if (top1 > 0) { 
-			top1--; 
-			arr[top1] = x; 
-		} 
-		else { 
-			cout << "Stack Overflow"
-				<< " By element : " << x << endl; 
-			return; 
-		} 
-	} 
+	twoStacks(int n) { 
+	size = n; 
+	arr = new int[n]; 
+	top1 = n / 2 + 1; 
+	top2 = n / 2; 
+} 
+void push1(int x){ 
+	if (top1 > 0) { 
+	top1--; 
+	arr[top1] = x; 
+} 
+else { 
+cout << "Stack Overflow"<< " By element : " << x << endl; 
+return; 
+   }
+} 
+void push2(int x) { 
+if (top2 < size - 1) { 
+top2++; 
+arr[top2] = x; 
+} 
+else { 
+cout << "Stack Overflow"<< " By element : " << x << endl; 
+return; 
+   } 
+} 
+int pop1() { 
+if (top1 <= size / 2) { 
+int x = arr[top1]; 
+top1++; 
+return x; 
+} 
+else { 
+cout << "Stack UnderFlow"; 
+exit(1); 
+   } 
+} 
 
-	void push2(int x) 
-	{ 
-		if (top2 < size - 1) { 
-			top2++; 
-			arr[top2] = x; 
-		} 
-		else { 
-			cout << "Stack Overflow"
-				<< " By element : " << x << endl; 
-			return; 
-		} 
-	} 
-
-	int pop1() 
-	{ 
-		if (top1 <= size / 2) { 
-			int x = arr[top1]; 
-			top1++; 
-			return x; 
-		} 
-		else { 
-			cout << "Stack UnderFlow"; 
-			exit(1); 
-		} 
-	} 
-
-	int pop2() 
-	{ 
-		if (top2 >= size / 2 + 1) { 
-			int x = arr[top2]; 
-			top2--; 
-			return x; 
-		} 
-		else { 
-			cout << "Stack UnderFlow" << endl; 
-			exit(1); 
+int pop2(){ 
+if (top2 >= size / 2 + 1) { 
+int x = arr[top2]; 
+top2--; 
+return x; 
+} 
+else { 
+cout << "Stack UnderFlow" << endl; 
+		          exit(1); 
 		} 
 	} 
 }; 
-
 int main() 
 { 
 	twoStacks ts(5); 
