9a6c15e878bad1c2e83578b07539dc575391a666
Surya Prakash Yadav <suryaprakash8707yadav@gmail.com>
2025-03-04 23:38:07 +0530

    Pascal's Triangle

--- GeekForGeeks/PascalsTriangle.cpp
+++ GeekForGeeks/PascalsTriangle.cpp
@@ -6,22 +6,21 @@ int binomialCoeff(int n, int k);
 void printPascal(int n)
 {
     for (int line = 0; line < n; line++)
-    {
+      {
         for (int i = 0; i <= line; i++)
-            cout <<" "<< binomialCoeff(line, i);
+        cout <<" "<< binomialCoeff(line, i);
         cout <<"\n";
-    }
-}
-int binomialCoeff(int n, int k)
-{
+     }
+ }
+ int binomialCoeff(int n, int k){
     int res = 1;
     if (k > n - k)
     k = n - k;
     for (int i = 0; i < k; ++i)
-    {
-        res *= (n - i);
-        res /= (i + 1);
-    }
+ {
+     res *= (n - i);
+     res /= (i + 1);
+ }
     
     return res;
 }
