d3aab14ee5c689544d1fd59b633e3dd5ce7f980d
Surya Prakash Yadav <suryaprakash8707yadav@gmail.com>
2025-03-19 18:39:28 +0530

    Dificulty 203

--- Codechef/Rating 0 - 500/Dificulty203.cpp
+++ Codechef/Rating 0 - 500/Dificulty203.cpp
@@ -1,4 +1,4 @@
-Cricket World Cup Qualifier
+/* Cricket World Cup Qualifier
 
 The cricket World Cup has started in Chefland. There are many teams participating in the group stage matches. Any team that scores 
 12 or more points in the group stage matches qualifies for the next stage.
@@ -13,7 +13,7 @@ Output Yes, if the team has qualified for the next stage, and No otherwise.
 You may print each character of the string in uppercase or lowercase (for example, the strings YES, yEs, yes, and yeS will all be treated as identical).
 
 Constraints
-1≤X≤20
+1≤X≤20  */
 
 #include <bits/stdc++.h>
 using namespace std;
