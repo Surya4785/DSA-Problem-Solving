955510a463f468267e5a6a7cfa99d15cf651d203
Surya Prakash Yadav <suryaprakash8707yadav@gmail.com>
2025-03-26 00:04:58 +0530

    Audible Range

--- Codechef/Rating 0 - 500/Dificulty279.cpp
+++ Codechef/Rating 0 - 500/Dificulty279.cpp
@@ -1,3 +1,19 @@
+/* Audible Range
+Chef's dog binary hears frequencies starting from 67 Hertz to 45000 Hertz (both inclusive).
+If Chef's commands have a frequency of X Hertz, find whether binary can hear them or not.
+
+Input Format
+The first line of input will contain a single integer T, denoting the number of test cases.
+Each test case consists of a single integer X - the frequency of Chef's commands in Hertz.
+	
+Output Format
+For each test case, output on a new line YES, if binary can hear Chef's commands. Otherwise, print NO.
+The output is case-insensitive. Thus, the strings YES, yes, yeS, and Yes are all considered the same.
+
+Constraints
+1≤T≤10^4
+1≤X≤10^6 */
+
 #include <bits/stdc++.h>
 using namespace std;
 
