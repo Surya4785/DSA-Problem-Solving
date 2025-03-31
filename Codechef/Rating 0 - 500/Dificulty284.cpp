3d997d9810a5a964b2078d4b25d21c911e90adaa
Surya Prakash Yadav <suryaprakash8707yadav@gmail.com>
2025-03-31 20:20:53 +0530

    Reach the Target

--- Codechef/Rating 0 - 500/Dificulty284.cpp
+++ Codechef/Rating 0 - 500/Dificulty284.cpp
@@ -1,3 +1,21 @@
+/* Reach the Target
+There is a cricket match going on between two teams A and B.
+Team B is batting second and got a target of X runs. Currently, team B has scored Y runs. Determine how many more runs Team 
+B should score to win the match.
+
+Note: The target score in cricket matches is one more than the number of runs scored by the team that batted first.
+
+Input Format
+The first line of input will contain a single integer T, denoting the number of test cases.
+Each test case consists of two space-separated integers X and Y, the target for team B and the current score of team B respectively.
+	
+Output Format
+For each test case, output how many more runs team B should score to win the match.
+
+Constraints
+1≤T≤10
+50≤Y<X≤200  */
+
 include <bits/stdc++.h>
 using namespace std;
 
