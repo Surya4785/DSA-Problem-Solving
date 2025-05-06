abaa5cf539a69ac9455c5c33a93e06ab2a942a5e
Surya Prakash Yadav <suryaprakash8707yadav@gmail.com>
2025-05-06 22:35:38 +0530

    Playlist

---
+++ Codechef/Rating 0 - 500/Dificulty489.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t, n, x;
+    cin >> t;
+    while (t--) {
+        cin >> n >> x;
+        cout << n / (3 * x) << endl;
+    }
+    return 0;
+}
