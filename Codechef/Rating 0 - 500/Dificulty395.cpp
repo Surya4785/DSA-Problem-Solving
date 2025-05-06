e80cf5b530fa9d34c5de9d7020facb43741f2f95
Surya Prakash Yadav <suryaprakash8707yadav@gmail.com>
2025-05-06 21:57:14 +0530

    Reach Home

---
+++ Codechef/Rating 0 - 500/Dificulty395.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	for(int i=0; i<t; i++){
+	    int x,y;
+	    cin>>x>>y;
+	    if((5*x)>=y){
+	        cout<<"YES"<<endl;
+	    }
+	    else{
+	        cout<<"NO"<<endl;
+	    }
+	}
+}
