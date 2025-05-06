d886b82897482bbf869694e2b718e62490fdc771
Surya Prakash Yadav <suryaprakash8707yadav@gmail.com>
2025-05-06 22:25:21 +0530

    Codechef Airlines

---
+++ Codechef/Rating 0 - 500/Dificulty476.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, x, y, z;
+	cin >> t;
+	while(t--) {
+	    cin >> x >> y >> z;
+	    int seats = min((10 * x), y);
+	    cout << seats * z << "\n";
+	}
+	return 0;
+}
