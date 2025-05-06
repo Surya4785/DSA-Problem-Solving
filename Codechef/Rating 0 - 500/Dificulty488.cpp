7b3b6518fc1d92810cd5f085b8c64f7231eaca70
Surya Prakash Yadav <suryaprakash8707yadav@gmail.com>
2025-05-06 22:34:54 +0530

    Dominant Army

---
+++ Codechef/Rating 0 - 500/Dificulty488.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int armies[3];
+        for (int i = 0; i < 3; i++) {
+            cin >> armies[i];
+        }
+        sort(armies, armies + 3);
+        armies[0] + armies[1] < armies[2] ? cout << "YES" << endl : cout << "NO" << endl;
+    }
+    return 0;
+}
