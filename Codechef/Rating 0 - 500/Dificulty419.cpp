147897a05594210f989606aa83301f694b36d173
Surya Prakash Yadav <suryaprakash8707yadav@gmail.com>
2025-05-06 22:07:26 +0530

    Fill the Bucket

---
+++ Codechef/Rating 0 - 500/Dificulty419.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	for(int i=0; i<t; i++){
+	    int k,x;
+	    cin>>k>>x;
+	    cout<<(k-x)<<endl;
+	}
+}
